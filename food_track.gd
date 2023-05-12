d3f3b78319f82fd715661d616dcd7077d2808ff5
nuco-san <81429191+nuco-san@users.noreply.github.com>
2023-05-12 18:29:21 +0200

    fatto una build web

--- food_track.gd
+++ food_track.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+signal track_empty
+
+@export var track_id : int
 @export var moving_food : PackedScene
 @export var finishing_point : Marker2D
 
 @export var food_res : FoodRes
 @export var acceptable_id : String
-
-@export var spawn_time_interval : Vector2
 
 @export var sprite_rotation : float = 0.0
 
@@ -18,13 +19,6 @@ func _ready():
 	$FinishingPoint/FoodIcon.texture = load("res://03_Icone_EXPORT_ROTARY/" + acceptable_id + "_icona.png")
 	$FinishingPoint/FoodIcon.rotation_degrees = sprite_rotation
 	$FinishingPoint/ArrivedFoodSprite.rotation_degrees = sprite_rotation
-	prepare_spawn()
-
-
-func prepare_spawn():
-	randomize()
-	$Timer.wait_time = randf_range(spawn_time_interval.x, spawn_time_interval.y)
-	$Timer.start()
 
 
 func spawn_food():
@@ -34,11 +28,12 @@ func spawn_food():
 	var random_id = food_res.food_ids[randi() % food_res.food_ids.size()]
 	new_food.set_id(random_id)
 	new_food.set_sprite_rotation(sprite_rotation)
-	new_food.food_clicked.connect(_on_food_clicked)
+	new_food.food_clicked.connect(_on_food_avoided)
 	new_food.move(finishing_point)
 
 
 func _on_destination_area_entered(area):
+	track_empty.emit(track_id)
 	if acceptable_id == area.food_id:
 		print(area.food_id)
 		$FinishingPoint/FoodBackground.texture = load("res://04_Sprite_EXPORT_ROTARY/Slot_corretto.png")
@@ -46,11 +41,10 @@ func _on_destination_area_entered(area):
 		$FinishingPoint/FoodBackground.texture = load("res://04_Sprite_EXPORT_ROTARY/Slot_sbagliato.png")
 	$FinishingPoint/ArrivedFoodSprite.texture = load("res://04_Sprite_EXPORT_ROTARY/Sprite_prodotti_senzaombra_EXPORT_ROTARY/" + area.food_id + "_sprite.png")
 	area.destroy()
-	prepare_spawn()
 
 
-func _on_food_clicked():
-	prepare_spawn()
+func _on_food_avoided():
+	track_empty.emit(track_id)
 
 
 func _on_clear_button_pressed():
