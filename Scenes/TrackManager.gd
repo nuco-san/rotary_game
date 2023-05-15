13898cb1d69b6b7558a6045f4aa6b82b5b6739db
WAM_nic <>
2023-05-15 15:42:52 +0200

    risolto di nuovo :D il problema dello spawn contemporaneo

--- Scenes/TrackManager.gd
+++ Scenes/TrackManager.gd
@@ -10,6 +10,8 @@ export(Vector2) var spawn_time_interval
 var track_1_empty = false
 var track_2_empty = false
 
+var is_spawning = false
+
 
 func _ready():
 	track_1.connect("track_empty", self, "_on_track_empty")
@@ -22,14 +24,17 @@ func first_spawn():
 
 
 func _on_track_empty(id):
-	randomize()
-	$Timer.wait_time = rand_range(spawn_time_interval.x, spawn_time_interval.y)
-	$Timer.start()
-	yield($Timer, "timeout")
-	if id == 1:
-		track_2.spawn_food()
-	if id == 2:
-		track_1.spawn_food()
+	if not is_spawning:
+		is_spawning = true
+		randomize()
+		$Timer.wait_time = rand_range(spawn_time_interval.x, spawn_time_interval.y)
+		$Timer.start()
+		yield($Timer, "timeout")
+		if id == 1:
+			track_2.spawn_food()
+		if id == 2:
+			track_1.spawn_food()
+		is_spawning = false
 
 
 
