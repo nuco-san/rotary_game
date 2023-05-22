75a4b779a3546ba13e2163ee754bdf4131f3cf35
WAM_nic <>
2023-05-22 12:08:56 +0200

    implementato grafica nuova

--- Scenes/PauseMenu.gd
+++ Scenes/PauseMenu.gd
@@ -3,6 +3,13 @@ extends Node2D
 var pressed_buttons_number := 0
 
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		$PauseMenuPanel.show()
+		get_tree().paused = true
+
+
+
 func pause_button_pressed():
 	pressed_buttons_number += 1
 	if pressed_buttons_number == 4:
--- Scenes/UI_elements/UIButtonLevelSelection.gd
+++ Scenes/UI_elements/UIButtonLevelSelection.gd
@@ -5,6 +5,9 @@ export(int) var minigame_number
 signal minigame_selected
 
 
+func _ready():
+	$AnimationPlayer.play("underline_anim")
+
 
 func _on_LevelSelectionButton_pressed():
 	$AudioStreamPlayer.play()
