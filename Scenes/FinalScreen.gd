a9764fdf3db421530e1398a3a40c8fe108494a0c
WAM_nic <>
2023-05-22 12:18:50 +0200

    implementato final screen

---
+++ Scenes/FinalScreen.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_button_pressed():
+	Global.restart_game()
--- Scenes/Global.gd
+++ Scenes/Global.gd
@@ -22,13 +22,12 @@ func next_minigame():
 	current_round = 1
 	if current_game_mode == game_modes.STORY:
 		if current_minigame == 3:
-			get_tree().change_scene("res://Scenes/TItleScreen.tscn")
-			current_minigame = 1
+			get_tree().change_scene("res://Scenes/FinalScreen.tscn")
 		else:
 			current_minigame += 1
 			get_tree().change_scene("res://Scenes/Minigiochi/Minigioco_" + str(current_minigame) + ".tscn")
 	if current_game_mode == game_modes.SELECTION:
-		get_tree().change_scene("res://Scenes/TItleScreen.tscn")
+		get_tree().change_scene("res://Scenes/FinalScreen.tscn")
 
 
 func increase_round():
@@ -44,4 +43,16 @@ func load_minigame(number):
 func update_total_score():
 	total_score += partial_score
 	partial_score = 0
+
+
+func restart_game():
+	get_tree().change_scene("res://Scenes/TItleScreen.tscn")
+	current_minigame = 1
+	current_round = 1
+	current_game_mode = game_modes.STORY
+	total_score = 0
+	partial_score = 0
+
+
+
 
