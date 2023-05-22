78a0c01e84fe6f06e32636f17d93a8d5ca0b81e9
WAM_nic <>
2023-05-22 11:55:49 +0200

    sostituito grafiche titlescreen e pause menu

--- Scenes/TItleScreen.gd
+++ Scenes/TItleScreen.gd
@@ -5,15 +5,26 @@ func start_story_mode():
 	Global.load_minigame(1)
 	Global.current_game_mode = Global.game_modes.STORY
 
-func show_level_selection():
-	$MainButtons.hide()
-	$LevelSelectionButtons.show()
 
 func start_selected_minigame(number):
 	Global.load_minigame(number)
 	Global.current_game_mode = Global.game_modes.SELECTION
 
 
-func hide_level_selection():
-	$MainButtons.show()
-	$LevelSelectionButtons.hide()
+func show_titlescreen():
+	$TitleScreenUI.show()
+	$SelezioneUI.hide()
+	$CreditsUI.hide()
+
+
+func show_level_selection():
+	$TitleScreenUI.hide()
+	$SelezioneUI.show()
+
+
+func show_credits():
+	$TitleScreenUI.hide()
+	$CreditsUI.show()
+
+
+
--- Scenes/UI_elements/UIButton.gd
+++ Scenes/UI_elements/UIButton.gd
@@ -3,6 +3,9 @@ extends Button
 
 signal button_pressed
 
+func _ready():
+	$AnimationPlayer.play("button_highlight")
+
 
 func btn_pressed():
 	$AudioStreamPlayer.play()
